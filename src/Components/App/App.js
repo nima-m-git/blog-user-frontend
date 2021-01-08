e03e5ba8480edbedc1fd17bcfb6ff5ebfdf19cda
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2021-01-08 11:54:52 -0500

    header bar merged with links

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { useState } from "react";
 
 import "./App.scss";
@@ -13,53 +13,51 @@ function App() {
 
   return (
     <div className="app">
-      <Link to="/">
-        <header>
-          <h1>Blog Home</h1>
-        </header>
-      </Link>
       <LoginBar {...{ token }} {...{ setToken }} />
 
-      <Switch>
-        <Route path="/" exact>
-          <Menu {...{ setErrors }} />
-        </Route>
-        <Route path="/posts/:postId">
-          <PostPage
-            {...{ token }}
-            {...{ setErrors }}
-            {...{ setMessage }}
-            {...{ username }}
-          />
-        </Route>
-        <Route path="/login">
-          <Login {...{ setToken }} {...{ setUsername }} />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
+      <div className="container">
+        {/* Routes */}
+        <Switch>
+          <Route path="/" exact>
+            <Menu {...{ setErrors }} />
+          </Route>
+          <Route path="/posts/:postId">
+            <PostPage
+              {...{ token }}
+              {...{ setErrors }}
+              {...{ setMessage }}
+              {...{ username }}
+            />
+          </Route>
+          <Route path="/login">
+            <Login {...{ setToken }} {...{ setUsername }} />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
 
-      <div className="message-container">
-        {errors?.length > 0 && (
-          <div className="errors">
-            <h4 className="err-title">Errors: </h4>
-            {errors.map((error, i) => (
-              <div key={i}>- {error}</div>
-            ))}
-            <div className="ok-btn">
-              <button onClick={() => setErrors([])}>OK</button>
+        <div className="message-container">
+          {errors?.length > 0 && (
+            <div className="errors">
+              <h4 className="err-title">Errors: </h4>
+              {errors.map((error, i) => (
+                <div key={i}>- {error}</div>
+              ))}
+              <div className="ok-btn">
+                <button onClick={() => setErrors([])}>OK</button>
+              </div>
             </div>
-          </div>
-        )}
-        {message?.length > 0 && (
-          <div className="message">
-            {message}
-            <div className="ok-btn">
-              <button onClick={() => setMessage(null)}>OK</button>
+          )}
+          {message?.length > 0 && (
+            <div className="message">
+              {message}
+              <div className="ok-btn">
+                <button onClick={() => setMessage(null)}>OK</button>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
--- src/Components/App/LoginBar.js
+++ src/Components/App/LoginBar.js
@@ -3,32 +3,39 @@ import { Link } from "react-router-dom";
 import "./LoginBar.scss";
 
 const LoginBar = ({ token, setToken }) => (
-  <nav className="login-bar">
-    {token ? (
-      <div className="login-links">
-        <div className="link" onClick={() => setToken(null)}>
-          Logout
+  <header className="header">
+    <Link to="/">
+      <header>
+        <h1>Blog</h1>
+      </header>
+    </Link>
+    <nav className="login-bar">
+      {token ? (
+        <div className="login-links">
+          <div className="link" onClick={() => setToken(null)}>
+            Logout
+          </div>
         </div>
-      </div>
-    ) : (
-      <div className="login-links">
-        <a
-          href={process.env.REACT_APP_ADMIN_URL}
-          className="link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Admin
-        </a>
-        <Link className="link" to="/login">
-          Login
-        </Link>
-        <Link className="link" to="/signup">
-          Signup
-        </Link>
-      </div>
-    )}
-  </nav>
+      ) : (
+        <div className="login-links">
+          <a
+            href={process.env.REACT_APP_ADMIN_URL}
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Admin
+          </a>
+          <Link className="link" to="/login">
+            Login
+          </Link>
+          <Link className="link" to="/signup">
+            Signup
+          </Link>
+        </div>
+      )}
+    </nav>
+  </header>
 );
 
 export default LoginBar;
